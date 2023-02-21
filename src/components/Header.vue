672ae79d3a2fcdc00a11e3c68134ee8c78ca4a36
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-02-21 17:47:56 +0100

    header responsive

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,15 @@
 <template>
-    <template>
-        <div class="my-auto grid grid-cols-5 gap-5 bg-orange-100 text-black">
-            <a href="/">
-                <img src="logo.svg" alt="logo">
-            </a>
-            <RouterLink class="pt-6 text-xl font-bold xl:text-3xl" to="/Apropos">A propos</RouterLink>
-            <RouterLink class="pt-6 text-xl font-bold xl:text-3xl" to="/">Contact</RouterLink>
-            <RouterLink class="pt-6 text-xl font-bold xl:text-3xl" to="/Mesprojets">Mes projets</RouterLink>
+    <template class="bg-orange-100">
+        <a href="/">
+            <img class="w-24 ml-10 mt-5 " src="logo.svg" alt="logo">
+        </a>
+        <div class=" flex ml-auto gap-10 bg-orange-100 text-black">
+
+
+            <RouterLink class="pt-6 text-xl font-inria xl:text-2xl" to="/Apropos">A propos</RouterLink>
+            <RouterLink class="pt-6 text-xl font-inria xl:text-2xl" to="/">Contact</RouterLink>
+            <RouterLink class="pt-6 text-xl font-inria xl:text-2xl" to="/Mesprojets">Mes projets</RouterLink>
+
 
         </div>
     </template>
--- src/components/Headermobile.vue
+++ src/components/Headermobile.vue
@@ -1,5 +1,6 @@
 <template>
     <header class="sticky top-0 z-50 flex h-16 w-screen items-center bg-orange-100">
+        <img class="w-10 ml-3 mt-2 " src="logo.svg" alt="logo">
         <MenuAlt1Icon class="absolute right-4 h-8 w-8" @click="MenuOpen = !MenuOpen" v-if="!MenuOpen" />
         <XIcon class="absolute right-4 z-50 h-8 w-8" @click="MenuOpen = !MenuOpen" v-if="MenuOpen" />
         <a href="/">
@@ -7,7 +8,7 @@
         <nav class="absolute left-0 top-0 bottom-0 right-0 h-screen -translate-x-full bg-orange-100 opacity-0"
             :class="MenuOpen && 'translate-x-0 opacity-100'">
             <ul
-                class="flex h-full flex-col items-center justify-center gap-14 text-left font-work-sans text-3xl font-bold text-black">
+                class="flex h-full flex-col items-center justify-center gap-14 text-left font-inria text-2xl font-bold text-black">
                 <li>
                     <RouterLink to="/" @click="MenuOpen = !MenuOpen">Accueil</RouterLink>
                 </li>
