874d9816911f126034eaef7812f4f061a64f3421
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-02-21 17:48:32 +0100

    footer et début page accueil

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,8 +1,11 @@
 <template>
-    <footer class="bg-orange-100">
-        <p>@Toutdroitréservé</p>
-        <p>PORTFOLIO2023</p>
-        <a href="https://www.instagram.com/morgane.19_c/"><img src="instagram.svg" alt="réseau instagram"></a>
-        <a href="https://twitter.com/MorganeCHAMBER2"><img src="twitter.svg" alt="réseau twitter"></a>
+    <footer class="bg-orange-100 grid grid-cols-3 pt-5 ">
+        <p class="justify-left font-inria">@Toutdroitréservé</p>
+        <p class="m-auto font-inria">PORTFOLIO2023</p>
+
+        <div class="flex w-20 gap-5 ml-auto mr-7 mb-3">
+            <a href="https://www.instagram.com/morgane.19_c/"><img src="instagram.svg" alt="réseau instagram"></a>
+            <a href="https://twitter.com/MorganeCHAMBER2"><img src="twitter.svg" alt="réseau twitter"></a>
+        </div>
     </footer>
 </template>
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,13 +1,26 @@
 <template>
   <main class="bg-orange-100">
-    <img src="cathedrale.png" alt="image home page">
-    <h1 class="text-2xl">COMMUNICATION</h1>
-    <h4 class="font-montserrat text-beige">Morgane CHAMBERLIN</h4>
-    <h2>MES PROJETS MES PROJETS MES PROJETS</h2>
-    <img src="cartedevisite.png" alt="projet carte de visite">
-    <img src="citadellebesancon.png" alt="projet citadelle">
-    <img src="puma-happysocks.png" alt="Projet affiche">
-    <img src="emorphoz.png" alt="projet emorphoz">
+
+    <div class="xl:flex xl:justify-center pb-28 ">
+
+      <img class="w-1/4 xs:w-1/2" src="cathedrale.png" alt="image home page">
+      <div class="xl:mt-24 xl:ml-8">
+        <h1 class="xl:text-6xl font-inria">COMMUNICATION</h1>
+        <h4 class="font-inria xl:text-2xl xl:ml-72 ">Morgane CHAMBERLIN</h4>
+        <img class="xl:w-36 xl:ml-48 xl:mt-12 " src="logo.svg" alt="logo">
+
+      </div>
+
+    </div>
+
+    <h2 class="z-20 mx-10">MES PROJETS MES PROJETS MES PROJETS</h2>
+    <div class=" -mt-3 mb-20 h-28 w-full z-0 bg-yellow-700"></div>
+    <div class="xl:grid xl:grid-cols-2 justify-items-center ">
+      <img src="cartedevisite.png" alt="projet carte de visite">
+      <img src="citadellebesancon.png" alt="projet citadelle">
+      <img src="puma-happysocks.png" alt="Projet affiche">
+      <img src="emorphoz.png" alt="projet emorphoz">
+    </div>
 
     <h2>CONTACT CONTACT CONTACT</h2>
 
