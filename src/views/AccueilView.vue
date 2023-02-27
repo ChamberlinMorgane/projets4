17c77eb370db6f391f73468a7d18b298f4e46085
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-02-27 20:25:13 +0100

    continuité page accuiel

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,3 +1,6 @@
+
+
+
 <template>
   <main class="bg-orange-100">
 
@@ -13,21 +16,47 @@
 
     </div>
 
-    <h2 class="z-20 mx-10">MES PROJETS MES PROJETS MES PROJETS</h2>
-    <div class=" -mt-3 mb-20 h-28 w-full z-0 bg-yellow-700"></div>
+    <h2 class="mx-48 text-6xl ">MES PROJETS MES PROJETS MES PROJETS </h2>
+    <div class=" -mt-20 mb-16 h-28 w-full z-0 bg-lime-800 opacity-40"></div>
     <div class="xl:grid xl:grid-cols-2 justify-items-center ">
-      <img src="cartedevisite.png" alt="projet carte de visite">
-      <img src="citadellebesancon.png" alt="projet citadelle">
+      <div class="grid grid-cols-1">
+        <div class=" -mb-20 opacity-10 hover:opacity-50 h-96 w-full z-0 bg-orange-50 hover:bg-black ">
+        </div>
+        <img class="-mt-72" src="cartedevisite.png" alt="projet carte de visite">
+        <div class=" h-1 w-1/2 m-auto mt-3 bg-black"></div>
+        <p class="xl:text-2xl font-inria m-auto">CARTE DE VISITE</p>
+        <p class="xl:text-2xl font-inria rotate-90 -mr-20 ml-auto  ;">CRÉATION</p>
+      </div>
+
+
+      <div class="grid grid-cols-1">
+        <div class=" -mb-20 opacity-10 hover:opacity-50 h-96 w-full z-0 bg-orange-50 hover:bg-black ">
+        </div>
+        <img class="-mt-72" src="citadellebesancon.png" alt="citadelle de besancon">
+        <div class=" h-1 w-1/2 m-auto mt-3 bg-black"></div>
+        <p class="xl:text-2xl font-inria m-auto">Citadelle de Besançon</p>
+        <p class="xl:text-2xl font-inria rotate-90 -mr-20 ml-auto  ;">ANIMATION</p>
+      </div>
       <img src="puma-happysocks.png" alt="Projet affiche">
       <img src="emorphoz.png" alt="projet emorphoz">
     </div>
 
     <h2>CONTACT CONTACT CONTACT</h2>
+    <form id="ProjetS4">
+      <div class="">
+        <input class=" bg-lime-800 opacity-40" id="Nom" name="Nom" placeholder="Nom">
+        <input class=" bg-lime-800 opacity-40" id="Email" name="Email" placeholder="Email">
+        <input class=" bg-lime-800 opacity-40" id="Message" name="Message" placeholder="Message">
+        <button>Envoyer</button>
+      </div>
+
+    </form>
+    <div class=" h-96 w-full z-0 bg-lime-800 opacity-40"></div>
 
 
   </main>
 </template>
 
 <script>
-export default {};
+
 </script>
