30324e14de63abb3288fd5915a27c60f9e66d6ea
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-03 15:44:24 +0100

    début de page de contact

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <form>
+            <div class="grid grid-cols-1 xl:w-1/4 ml-48 -mb-80">
+                <input class="text-black bg-transparent border-b-2 border-black" type="text" name="Nom" id="nom">
+                <input class="text-black bg-transparent border-b-2 border-black" type="text" name="Email" id="email">
+                <textarea class="text-black bg-transparent border-b-2 border-black" name="Message" id="message" cols="30"
+                    rows="10"></textarea>
+            </div>
+
+        </form>
+    </main>
+</template>
