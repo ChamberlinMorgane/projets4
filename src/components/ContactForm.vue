378e8425146183ed701a41d97a69ecc71195a335
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-11 20:14:41 +0100

    mise en place formulaire contact fonctionnelle

--- src/components/ContactForm.vue
+++ src/components/ContactForm.vue
@@ -1,13 +1,15 @@
 <template>
     <main>
-        <form>
-            <div class="grid grid-cols-1 xl:w-1/4 ml-48 -mb-80">
-                <input class="text-black bg-transparent border-b-2 border-black" type="text" name="Nom" id="nom">
-                <input class="text-black bg-transparent border-b-2 border-black" type="text" name="Email" id="email">
-                <textarea class="text-black bg-transparent border-b-2 border-black" name="Message" id="message" cols="30"
-                    rows="10"></textarea>
+        <form action="https://formspree.io/f/xvonqaay" method="POST">
+            <div class="grid grid-cols-1 xl:w-1/4 ml-48 -mb-80 z-30">
+                <input class=" text-black bg-transparent border-b-2 border-black" placeholder="Nom" type="text" name="Nom"
+                    id="nom">
+                <input class="text-black bg-transparent border-b-2 border-black" placeholder="Email" type="text"
+                    name="Email" id="email">
+                <textarea class="text-black bg-transparent border-b-2 border-black" placeholder="Message" name="Message"
+                    id="message" cols="30" rows="10"></textarea>
+                <button class="border-2 border-black w-20 h-10" type="submit">Envoyer</button>
             </div>
-
         </form>
     </main>
 </template>
