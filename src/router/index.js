db234aec9c6c4d35294c0b5295a70e670160748d
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-11 13:23:38 +0100

    Router link page projet

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AccueilView from '../views/AccueilView.vue'
 import AproposView from '../views/AproposView.vue'
+import AfficheView from '../views/AfficheView.vue'
+import CartevisiteView from '../views/CartevisiteView.vue'
+import CitadelleView from '../views/CitadelleView.vue'
+import EmorphozView from '../views/EmorphozView.vue'
 import MesprojetsView from '../views/MesprojetsView.vue'
 
 const router = createRouter({
@@ -9,6 +13,10 @@ const router = createRouter({
     { path: '/', name: 'Accueil', component: AccueilView },
     { path: '/Apropos', name: 'Apropos', component: AproposView },
     { path: '/Mesprojets', name: 'Mesprojets', component: MesprojetsView },
+    { path: '/Affiche', name: 'Affiche', component: AfficheView },
+    { path: '/Cartevisite', name: 'Cartevisite', component: CartevisiteView },
+    { path: '/Citadelle', name: 'Citadelle', component: CitadelleView },
+    { path: '/Emorphoz', name: 'Emorphoz', component: EmorphozView },
     // ici les autre routes
   ]
 })
