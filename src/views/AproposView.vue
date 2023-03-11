16cf5032828fe847b5d9e2d4adefce8bf46cfec3
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-11 20:15:15 +0100

    commencement css a propos

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -7,28 +7,41 @@ import Chart from '@/components/Chart.vue'
 <template>
     <main class="bg-orange-100">
 
-        <h2>PRÉSENTATION</h2>
+        <h2 class="font-caveat xl:text-6xl xl:ml-20  mb-16 mt-20 text-xl justify-center">PRÉSENTATION</h2>
 
-        <img class="w-20 xl:w-64 " src="photo-moi.png" alt="Photo de moi">
-        <p>Je suis Actuellement en deuxième année d’un Bachelor Métiers du Multimédia et de l’Internet, je suis spécialisée
-            en parcours Communication et UX Design.
+        <div class="grid grid-cols-1 xl:flex xl:space-x-20 xl:m-20">
+            <img class="w-48 xl:w-72" src="photo-moi.png" alt="Photo de moi">
+            <div class="text-center flex flex-col p-20">
+                <p>Je suis Actuellement en deuxième année d’un Bachelor Métiers du Multimédia et de
+                    l’Internet, je suis
+                    spécialisée
+                    en parcours Communication et UX Design.</p>
 
-            Ayant toujours aimer le domaine de la communication digitale j’ai décidé de me spécialisée dans ce parcours et
-            de faire la fin de ma formation en alternance pour avoir en plus de la théorie du concret et quelque chose de
-            professionnalisant.
+                <p>
+                    Ayant toujours aimer le domaine de la communication digitale j’ai décidé de me spécialisée dans ce
+                    parcours
+                    et
+                    de faire la fin de ma formation en alternance pour avoir en plus de la théorie du concret et quelque
+                    chose
+                    de
+                    professionnalisant.</p>
+                <p>
+                    acquis une solide expérience en matière de marketing numérique, de stratégie de contenu et de gestion de
+                    réseaux
+                    sociaux, je voudrais par la suite me spécialiser dans ce domaine.
 
-            acquis une solide expérience en matière de marketing numérique, de stratégie de contenu et de gestion de réseaux
-            sociaux, je voudrais par la suite me spécialisée me spécialiser dans ce domaine.
+                    Je suis toujours à l’affut des dernières tendances en matière de communication digitale. Je veux après
+                    l’obtention de mon BUT rentrer dans la vie professionnelle ou faire un master en communication et
+                    marketing.
+                </p>
+            </div>
+        </div>
 
-            Je suis toujours à l’affut des dernières tendances en matière de communication digitale. Je veux après
-            l’obtention de mon BUT rentrer dans la vie professionnelle ou faire un master dans la communication et
-            marketing.
-        </p>
 
 
-        <h2>MES COMPÉTENCES</h2>
-        <div class="xl:flex space-x-14 ">
-            <div class="grid grid-cols-1">
+        <h2 class="font-caveat xl:text-6xl xl:ml-20  mb-16">MES COMPÉTENCES</h2>
+        <div class="xl:flex items-center ">
+            <div class="grid grid-cols-1 ">
                 <img class="xl:w-32" src="Figma.svg" alt="Figma">
                 <p class="mx-auto">Figma</p>
             </div>
