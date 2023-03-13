a24d1578706b5573f689f128d41e5756ae3d1924
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-13 16:35:44 +0100

    lien vers l'accueil

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
         <a href="/">
             <img class="w-24 ml-10 mt-5 " src="logo.svg" alt="logo">
         </a>
-        <div class=" flex ml-auto gap-10 bg-orange-100 text-black">
+        <div class=" flex ml-auto gap-10 mr-10 bg-orange-100 text-black">
 
 
             <RouterLink class="pt-6 text-xl font-inria xl:text-2xl" to="/Apropos">A propos</RouterLink>
