7085fac42e57e74381c7db34674d3b87bbd74b3e
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-14 18:11:57 +0100

    debut page projets

--- src/views/CitadelleView.vue
+++ src/views/CitadelleView.vue
@@ -1,1 +1,16 @@
-<template></template>
+
+
+<template>
+    <main>
+
+        <img class="w-full z-0 h-screen mt-10" src="ciel.jpg" alt="ciel">
+        <p class="absolute -mt-96">bonjour</p>
+
+        <img class="w-full h-screen opacity-70" src="chateau.jpg" alt="chateau">
+        <img class="w-full h-screen opacity-70" src="corbeau.jpg" alt="corbeau">
+        <img class="w-full h-screen opacity-70" src="fond.jpg" alt="fond">
+
+
+
+    </main>
+</template>
