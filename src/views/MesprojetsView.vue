7695f215ff8d6d2db80e0333cdcc824ef4f522d5
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-14 18:12:30 +0100

    page mes projets

--- src/views/MesprojetsView.vue
+++ src/views/MesprojetsView.vue
@@ -1,10 +1,114 @@
 <template class>
     <main>
-        <img class="z-10 mt-48" src="rectangle.jpg" alt="rectangle">
-        <img class="z-20 -mt-96 ml-28 w-48" src="ciel-projet.jpg" alt="ciel projet">
-        <p>LA CITADELLE DE BESANCON</p>
-        <p>Vectorisation et animation</p>
-        <button class=" border-2 p-2 border-black">En savoir plus</button>
-        <img src="citadelle-besancon.jpg" alt="citadelle besancon">
+
+        <div class=" flex w-full h-96 snap-x overflow-x-auto overflow-y-hidden pb-14 mt-10">
+            <div class="w-full shrink-0 rounded-lg shadow-xl ">
+                <img src="communication-1.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="communication-2.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="communication-3.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="communication-4.jpg" alt="image concert" />
+            </div>
+        </div>
+        <div class="grid grid-cols-1 xl:grid-cols-2 gap-20 place-items-center">
+
+            <div class="xl:max-w-xl max-w-xs bg-lime-800 bg-opacity-50 border border-gray-200 rounded-lg shadow ">
+                <a href="Citadelle">
+                    <img class="rounded-t-lg h-2/3 w-full" src="citadelle-besancon.jpg" alt="citadelle besancon" />
+                </a>
+                <div class="p-5">
+                    <a href="Citadelle">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black text-center font-inria">Animation
+                            Citadelle
+                            de
+                            Besançon</h5>
+                    </a>
+                    <p class="mb-3 font-normal font-montserrat text-gray-900 text-center">Cette animation a été faite dans
+                        le
+                        cadre de ma
+                        formation en MMi, il s’agit d’un travail en design. </p>
+
+
+                    <a href="Citadelle"
+                        class="inline-flex  px-3 py-2 text-sm font-medium font-montserrat text-center text-black bg-b-transparent border-2 border-black rounded-lg hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        EN SAVOIR PLUS
+
+                    </a>
+
+                </div>
+            </div>
+
+            <div class="xl:max-w-xl max-w-xs bg-lime-800 bg-opacity-50 border border-gray-200 rounded-lg shadow ">
+                <a href="Cartevisite">
+                    <img class="rounded-t-lg w-full h-2/3" src="cartedevisite.png" alt="carte de visite" />
+                </a>
+                <div class="p-5">
+                    <a href="Cartevisite">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black text-center font-inria">Création carte
+                            de visite</h5>
+                    </a>
+                    <p class="mb-3 font-normal font-montserrat text-gray-900 text-center">Carte de visite pour 3 magasins
+                        fait dans le cadre de ma formation en alternance à la Société de la Licorne. </p>
+
+
+                    <a href="Cartevisite"
+                        class="inline-flex  px-3 py-2 text-sm font-medium font-montserrat text-center text-black bg-b-transparent border-2 border-black rounded-lg hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        EN SAVOIR PLUS
+
+                    </a>
+
+                </div>
+            </div>
+
+            <div class="xl:max-w-xl max-w-xs bg-lime-800 bg-opacity-50 border border-gray-200 rounded-lg shadow ">
+                <a href="Affiche">
+                    <img class="rounded-t-lg h-2/3 w-full" src="puma-happysocks.png" alt="Affiche puma" />
+                </a>
+                <div class="p-5">
+                    <a href="Affiche">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black text-center font-inria">Affiche Puma,
+                            HappySocks, La Chaise Longue</h5>
+                    </a>
+                    <p class="mb-3 font-normal font-montserrat text-gray-900 text-center">Affiche fait dans le cadre de ma
+                        formation en alternance à la Société de la Licorne.</p>
+
+
+                    <a href="Affiche"
+                        class="inline-flex  px-3 py-2 text-sm font-medium font-montserrat text-center text-black bg-b-transparent border-2 border-black rounded-lg hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        EN SAVOIR PLUS
+
+                    </a>
+
+                </div>
+            </div>
+
+            <div class="xl:max-w-xl max-w-xs bg-lime-800 bg-opacity-50 border border-gray-200 rounded-lg shadow ">
+                <a href="Emorphoz">
+                    <img class="rounded-t-lg h-2/3 w-full" src="emorphoz.png" alt="Projet S3" />
+                </a>
+                <div class="p-5">
+                    <a href="Emorphoz">
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-black text-center font-inria">Projet Emorphoz
+                        </h5>
+                    </a>
+                    <p class="mb-3 font-normal font-montserrat text-gray-900 text-center">Projet fait dans le cadre de ma
+                        foramtion en MMI ou j'ai eu le rôle de Chargée de communication. </p>
+
+
+                    <a href="Emorphoz"
+                        class="inline-flex  px-3 py-2 text-sm font-medium font-montserrat text-center text-black bg-b-transparent border-2 border-black rounded-lg hover:bg-black hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        EN SAVOIR PLUS
+
+                    </a>
+
+                </div>
+            </div>
+        </div>
+
     </main>
 </template>
