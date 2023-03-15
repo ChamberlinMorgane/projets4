e11c28a7a90ea8f60e16a13327bd0fb4515c9a6f
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-15 20:30:24 +0100

    ajout lien mentions légales

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
     <footer class="bg-orange-100 grid grid-cols-3 pt-5 ">
         <p class="justify-left font-inria">@Toutdroitréservé</p>
-        <p class="m-auto font-inria">PORTFOLIO2023</p>
+        <div class="grid grid-cols-1">
+            <p class="m-auto font-inria">PORTFOLIO2023</p>
+            <router-link class="m-auto font-inria" to="/Mentionlegales">
+                <p>Mentions Légales</p>
+            </router-link>
+        </div>
 
         <div class="flex w-20 gap-5 ml-auto mr-7 mb-3">
             <a href="https://www.instagram.com/morgane.19_c/"><img src="../assets/instagram.svg" alt="réseau instagram"></a>
