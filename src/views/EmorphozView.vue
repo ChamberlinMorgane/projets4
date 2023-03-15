a1113f4ca65bc59afd3db17d76ea3f7662151027
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-15 20:30:33 +0100

    commencement page emorphoz

--- src/views/EmorphozView.vue
+++ src/views/EmorphozView.vue
@@ -1,1 +1,273 @@
-<template></template>
+
+
+<template>
+    <main>
+
+        <div class=" flex w-full h-96 snap-x overflow-x-auto overflow-y-hidden pb-48 mt-10 ">
+            <div class="w-full shrink-0 rounded-lg shadow-xl ">
+                <img src="../assets/mockup-citadelle.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="../assets/mockup-citadelle-2.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="../assets/mockup-citadelle-3.jpg" alt="image concert" />
+            </div>
+            <div class="w-full shrink-0 rounded-lg shadow-xl">
+                <img src="../assets/mockup-citadelle-4.jpg" alt="image concert" />
+            </div>
+        </div>
+
+        <h2 class="xl:-mt-52 z-40 font-inria xl:text-8xl text-center text-4xl -mt-60 text-white ">PROJET EMORPHOZ</h2>
+
+
+        <div class="bg-[url('../assets/ciel-filtre.jpg')] w-full mt-60">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    1
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl ">CONTEXT</h3>
+            </div>
+            <p class="mt-20 font-montserrat xl:text-xl pb-48 xl:ml-48 text-center ml-10 mr-10 z-10">Ce projet a été fait
+                dans le cadre d’un exercice pédagogique scolaire de S4. Dans ce projet, j'ai été chargée de communication
+                avec une autre de mes camarades. Le Sujet était de faire un site de générateur de NFT avec 4 autres de mes
+                camarades qui était dans différente pôle de fabrication de ce projet.
+            </p>
+        </div>
+
+        <div class="bg-[url('../assets/fond-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-16 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    2
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl">LE NOM</h3>
+                <div class="grid grid-cols-1 xl:grid-cols-2 pt-60 pb-20 space-x-14 ">
+                    <p class="pr-20">Le nom E-morphoz est composé en 2 parties. La première est le “E” rappelant la
+                        technologie, la
+                        nouveauté,
+                        le digital… Comme on le retrouve souvent dans “E-Sport” ou encore “E-Tech”. La deuxième partie
+                        “Morphoz”
+                        fait référence au mot métamorphose, on retrouve là-dedans le principe du générateur qui est
+                        d’assembler
+                        plusieurs éléments pour créer sa NFT. On retrouve aussi dans la définition de métamorphose l’idée de
+                        changement dans un organisme vivant comme un insecte. </p>
+                    <img class="w-28 xl:w-60 mt-10" src="../assets/logoemorphoz.svg" alt="logo emorphoz">
+                </div>
+            </div>
+
+        </div>
+
+
+        <div class="bg-[url('../assets/corbeau-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    3
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl  ">TYPOGRAPHIES UTILISÉE </h3>
+            </div>
+            <p class="mt-20 font-montserrat xl:text-xl pb-48 xl:ml-48 text-center ml-10 mr-10 z-10">Pour le choix de la
+                typographie
+
+                2 principales: Audiowide et Trap.
+
+
+                Pour la typographie Audiowide, elle a été utilisée pour les gros titres du site puisque c’est une
+                typographie très futuriste comprenant des courbes spécialement prévues pour créer cet effet. C’est pour cela
+                également qu’elle est utilisée pour la création du logo d’emorphoz.
+
+
+                Enfin, pour la typographie Trap elle a été utilisée pour tous les autres titres restants sur le site. En
+                effet, trap est une police très lisible, ce qui est parfait pour la compréhension. Également, elle dispose
+                d’une certaine originalité puisqu'il y a quelques fentes qui donnent cet aspect futuriste.
+                Elle a été utilisé pour tout les paragraphes vu qu’elle possède une très bonne lisibilité.
+            </p>
+            <div>
+                <img class="w-28 xl:w-60 mt-10" src="../assets/logoemorphoz.svg" alt="logo emorphoz">
+                <img src="" alt="">
+            </div>
+        </div>
+        <div class="bg-[url('../assets/fond-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    4
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl">Harmonie coloré</h3>
+            </div>
+            <div class="grid grid-cols-1 xl:grid-cols-2 pt-60 pb-20 space-x-14 ">
+                <p class="pr-20">Pour le choix de l’harmonie colorée
+
+                    Nous avons décidé de partir sur une harmonie basée sur le violet.
+                    En général, sur les différents sites internets, le violet est utilisé pour donner un aspect futuriste.
+
+
+                    Pour la couleur blanche, elle à été utilisée principalement pour la typographie. En effet c’est une
+                    couleur qui contraste très bien avec les couleurs sombres en fond comme le violet foncée sur le site.
+                    Ceci respecte donc les normes de contraste pour la lisibilité.
+
+                    Pour les couleurs violettes elle ont été utilisé pour créer le fond de notre site:
+                    en utilisant le violet le plus foncé comme couleur de fond et l’autre pour faire un effet de dégradé en
+                    utilisant la propriété “blur” dans Figma.
+
+                    Au niveau des 2 dernières couleurs, elles ont été appliquées comme couleur tonique pour inciter
+                    l’utilisateur de notre site à l’action.
+                    Par exemple, pour les boutons du site, une couleur tonique sera parfaite pour attirer l'œil </p>
+
+            </div>
+        </div>
+
+        <div class="bg-[url('../assets/fond-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    4
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl">CAHIER DES CHARGES</h3>
+            </div>
+            <div class="grid grid-cols-1 xl:grid-cols-2 pt-60 pb-20 space-x-14 ">
+                <p class="pr-20">Pour le choix de l’harmonie colorée
+
+                    Nous avons décidé de partir sur une harmonie basée sur le violet.
+                    En général, sur les différents sites internets, le violet est utilisé pour donner un aspect futuriste.
+
+
+                    Pour la couleur blanche, elle à été utilisée principalement pour la typographie. En effet c’est une
+                    couleur qui contraste très bien avec les couleurs sombres en fond comme le violet foncée sur le site.
+                    Ceci respecte donc les normes de contraste pour la lisibilité.
+
+                    Pour les couleurs violettes elle ont été utilisé pour créer le fond de notre site:
+                    en utilisant le violet le plus foncé comme couleur de fond et l’autre pour faire un effet de dégradé en
+                    utilisant la propriété “blur” dans Figma.
+
+                    Au niveau des 2 dernières couleurs, elles ont été appliquées comme couleur tonique pour inciter
+                    l’utilisateur de notre site à l’action.
+                    Par exemple, pour les boutons du site, une couleur tonique sera parfaite pour attirer l'œil </p>
+
+            </div>
+        </div>
+
+        <div class="bg-[url('../assets/fond-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    4
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl">COMMUNICATIONS</h3>
+            </div>
+            <div class="grid grid-cols-1 xl:grid-cols-2 pt-60 pb-20 space-x-14 ">
+                <p class="pr-20">Pour le choix de l’harmonie colorée
+
+                    Nous avons décidé de partir sur une harmonie basée sur le violet.
+                    En général, sur les différents sites internets, le violet est utilisé pour donner un aspect futuriste.
+
+
+                    Pour la couleur blanche, elle à été utilisée principalement pour la typographie. En effet c’est une
+                    couleur qui contraste très bien avec les couleurs sombres en fond comme le violet foncée sur le site.
+                    Ceci respecte donc les normes de contraste pour la lisibilité.
+
+                    Pour les couleurs violettes elle ont été utilisé pour créer le fond de notre site:
+                    en utilisant le violet le plus foncé comme couleur de fond et l’autre pour faire un effet de dégradé en
+                    utilisant la propriété “blur” dans Figma.
+
+                    Au niveau des 2 dernières couleurs, elles ont été appliquées comme couleur tonique pour inciter
+                    l’utilisateur de notre site à l’action.
+                    Par exemple, pour les boutons du site, une couleur tonique sera parfaite pour attirer l'œil </p>
+
+            </div>
+        </div>
+
+        <div class="bg-[url('../assets/fond-filtre.jpg')] w-full">
+
+            <div class="flex pt-20 xl:pl-32 pl-14 space-x-10 relative z-10">
+                <h3
+                    class="z-10 xl:h-20 xl:w-20 h-14 w-14 bg-transparent border-4 border-solid border-black text-center xl:text-7xl text-4xl rounded-xl hover:bg-black hover:text-white hover:border-white">
+                    4
+                </h3>
+                <h3 class="font-caveat xl:text-7xl z-10 text-4xl">CE QUE CA M'A APPORTÉ</h3>
+            </div>
+            <div class="grid grid-cols-1 xl:grid-cols-2 pt-60 pb-20 space-x-14 ">
+                <p class="pr-20">Pour le choix de l’harmonie colorée
+
+                    Nous avons décidé de partir sur une harmonie basée sur le violet.
+                    En général, sur les différents sites internets, le violet est utilisé pour donner un aspect futuriste.
+
+
+                    Pour la couleur blanche, elle à été utilisée principalement pour la typographie. En effet c’est une
+                    couleur qui contraste très bien avec les couleurs sombres en fond comme le violet foncée sur le site.
+                    Ceci respecte donc les normes de contraste pour la lisibilité.
+
+                    Pour les couleurs violettes elle ont été utilisé pour créer le fond de notre site:
+                    en utilisant le violet le plus foncé comme couleur de fond et l’autre pour faire un effet de dégradé en
+                    utilisant la propriété “blur” dans Figma.
+
+                    Au niveau des 2 dernières couleurs, elles ont été appliquées comme couleur tonique pour inciter
+                    l’utilisateur de notre site à l’action.
+                    Par exemple, pour les boutons du site, une couleur tonique sera parfaite pour attirer l'œil </p>
+
+            </div>
+        </div>
+
+
+
+        <div class="container mb-32 mt-11">
+            <h2 class="animation xl:text-6xl text-4xl">CONTACT CONTACT CONTACT CONTACT CONTACT</h2>
+            <div class=" -mt-20 h-28 w-full z-0 bg-pink-300 bg-opacity-20"></div>
+        </div>
+
+
+        <section id="contact">
+            <div class=" xl:p-8 xl:my-32 mt-32">
+                <ContactForm />
+            </div>
+        </section>
+
+
+    </main>
+</template>
+
+<script>
+import ContactForm from '@/components/ContactForm.vue'
+
+export default {
+    components: {
+        ContactForm
+    }
+}
+
+
+</script>
+
+<style>
+.container {
+    overflow: hidden;
+    position: absolute;
+    width: 100%;
+}
+
+.animation {
+    overflow: hidden;
+    white-space: nowrap;
+    animation: translate 20s linear infinite
+}
+
+@keyframes translate {
+
+    0% {
+        transform: translateX(-100%)
+    }
+
+    100% {
+        transform: translateX(100%)
+    }
+
+}
+</style>
