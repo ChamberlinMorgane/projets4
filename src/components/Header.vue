6ee8d632f3a187a0341222db20c8d3fae301463b
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-15 20:34:05 +0100

    changment lien header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,7 @@
 <template>
     <template class="bg-orange-100">
-        <a href="/">
-            <img class="w-24 ml-10 mt-5 " src="../assets/logo.svg" alt="logo">
-        </a>
+
+        <router-link class="" to="/"><img class="w-24 ml-10 mt-5 " src="../assets/logo.svg" alt="logo"></router-link>
         <div class=" flex ml-auto gap-10 mr-10 bg-orange-100 text-black">
 
 
