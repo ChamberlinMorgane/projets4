1943ccbcd3afd1e51001d486efc2421313fadf8f
Morgane <morgane.chamberlin@edu.univ-fcomte.fr>
2023-03-15 21:55:55 +0100

    faute a propos

--- src/views/AproposView.vue
+++ src/views/AproposView.vue
@@ -25,25 +25,18 @@
         <div class="grid grid-cols-1 xl:flex xl:space-x-20 xl:m-20">
             <img class="w-48 xl:w-72 m-auto mt-10" src="../assets/photo-moi.png" alt="Photo de moi">
             <div class="text-center flex flex-col p-20">
-                <p>Je suis Actuellement en deuxième année d’un Bachelor Métiers du Multimédia et de
-                    l’Internet, je suis
-                    spécialisée
-                    en parcours Communication et UX Design.</p>
+                <p>Je suis actuellement en deuxième année d’un Bachelor Métiers du Multimédia et de l'Internet, je suis
+                    spécialisée en parcours Communication et Design.</p>
 
                 <p>
-                    Ayant toujours aimer le domaine de la communication digitale j’ai décidé de me spécialisée dans ce
-                    parcours
-                    et
-                    de faire la fin de ma formation en alternance pour avoir en plus de la théorie du concret et quelque
-                    chose
-                    de
-                    professionnalisant.</p>
+                    Ayant toujours aimé le domaine de la communication digitale, j’ai décidé de me spécialiser dans ce
+                    parcours et de faire la fin de ma formation en alternance pour avoir en plus de la théorie du concret et
+                    quelque chose de professionnalisant.</p>
                 <p>
-                    acquis une solide expérience en matière de marketing numérique, de stratégie de contenu et de gestion de
-                    réseaux
-                    sociaux, je voudrais par la suite me spécialiser dans ce domaine.
+                    Ayant acquis une solide expérience en matière de marketing numérique, de stratégie de contenu et de
+                    gestion de réseaux sociaux, je voudrais par la suite me spécialiser dans ce domaine.
 
-                    Je suis toujours à l’affut des dernières tendances en matière de communication digitale. Je veux après
+                    Je suis toujours à l’affût des dernières tendances en matière de communication digitale. Je veux après
                     l’obtention de mon BUT rentrer dans la vie professionnelle ou faire un master en communication et
                     marketing.
                 </p>
